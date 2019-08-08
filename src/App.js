8fef803bb8810bc5ab7bb29fe0f26e123fc32a42
Max <mjann@ucsc.edu>
2019-08-08 16:43:03 -0700

    i

--- src/App.js
+++ src/App.js
@@ -2,25 +2,41 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      song: <iframe width="560" height="315" src="https://www.youtube.com/embed/1R0AUyvNc4Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+      songNum: 1,
+    }
+    this.changeSong=this.changeSong.bind(this)
+  }
+
+  changeSong(){
+    console.log("changing song")
+    if(this.state.songNum===1){
+      this.setState({
+        song: <iframe width="560" height="315" src="https://www.youtube.com/embed/Zz80CVr9NTs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+        songNum: 2,
+      })
+    }else{
+      this.setState({
+        song: <iframe width="560" height="315" src="https://www.youtube.com/embed/1R0AUyvNc4Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+        songNum: 1,
+      })
+    }
+    
+  }
+
+  render(){
+    return (
+      <div className="App">
+        {this.state.song}
+        <button onClick={this.changeSong}>Change Song</button>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/songs.js
@@ -0,0 +1,10 @@
+const songs = [
+    {
+        id: 1,
+        code: <iframe width="560" height="315" src="https://www.youtube.com/embed/1R0AUyvNc4Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+    },
+    {
+        id: 2,
+        code: <iframe width="560" height="315" src="https://www.youtube.com/embed/Zz80CVr9NTs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+    }
+]
